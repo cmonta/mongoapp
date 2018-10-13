fdee7c72c91b17cc8b2222e6323f3cba9fa87869
Cyril Montailler <cyril@macbook-pro-de-cyril.home>
2018-10-13 18:35:36 +0200

    Bootstrap added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded( {extended: true}));
 
+app.use(express.static('public'));
+
 app.use('/', routes);
 
 module.exports = app;
